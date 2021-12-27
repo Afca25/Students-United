9283226d988098e6ce4965c89223bbb304bfe6aa
Afca25 <afca25login@gmail.com>
2021-12-27 13:33:02 -0400

    Home styling done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import heroHome from '../Resources/heroHome.jpg'
 
 function Home() {
   return (
-    <div className='home__container'>
+    <div className='home__container' style={{
+      backgroundImage: `url(${heroHome})`,
+      backgroundSize: "cover",
+    }}>
       <div className="home__gradient">
         <section className="home__text-content">
           <h1 className="home__header">Students United</h1>
           <h2 className='home__sub-header'>Passion for learning</h2>
         </section>
-        <div className='home__btn'>
+        <div className='home__btn' id='cta'>
           <button className="btn primary">Enroll    Now</ button>
         </div>
       </div>
