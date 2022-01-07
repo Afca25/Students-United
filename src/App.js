6d0f751a7d9607b78124744918ca6a790b9c0552
Afca25 <afca25login@gmail.com>
2022-01-06 20:40:12 -0400

    React Router incorporated, basic nav links working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 //Styles
 import './App.scss'
 
 //Components
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-import Courses from './Components/Courses';
+import TopCourses from './Components/TopCourses';
 import AboutUs from './Components/AboutUs';
 import ContactUs from './Components/ContactUs';
+import ErrorPage from "./Components/ErrorPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-      <Courses />
-      <AboutUs />
-      <ContactUs />
-    </>
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/top-courses" element={<TopCourses />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import  logo from '../Resources/logo.jpg';
 
 function Navbar() {
+  let navigate = useNavigate()
+
   return (
-    <nav className='navbar__container'>
-      <div className="navbar__container__logo">
-        <a href="/">
-          <img src={logo} alt="logo" />
-        </a>
-      </div>
-      <div className="navbar__container__links">
-        <ul>
-          <li><a href="/">Courses</a></li>
-          <li><a href="/">Faculties</a></li>
-          <li><a href="/">About Us</a></li>
-        </ul>
-        <div className="navbar__container__button">
-          <button className="btn secondary">Contact Us</button>
+      <nav className='navbar__container'>
+        <div className="navbar__container__logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
-      </div>
-      
-    </nav>
+        <div className="navbar__container__links">
+          <ul>
+            <li><Link to="/top-courses">Top Courses</Link></li>
+            <li><Link to="/about-us">About Us</Link></li>
+          </ul>
+          <div className="navbar__container__button">
+            <button
+             className="btn secondary"
+             onClick={() => {
+               navigate("/contact-us")
+             }}
+             >Contact Us</button>
+          </div>
+        </div>
+        
+      </nav>
   )
 }
 
